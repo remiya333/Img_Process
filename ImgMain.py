9bbd2fce1e277bba6353593e5c1aebf5c25ebc58
Remiya <remiya333@gmail.com>
2020-06-22 22:01:43 +0530

    Added filter to sharpen

--- ImgMain.py
+++ ImgMain.py
@@ -3,6 +3,7 @@ img = Image.open("C:\Rem\Personal_Docs\IMG_20191008_183954.jpg")
 #filtered_Img=img.filter(ImageFilter.SHARPEN)
 filtered_Img = img.convert('L')
 filtered_Img.show()
+filtered_Img=img.filter(ImageFilter.SHARPEN)
 flip_img=img.transpose(Image.FLIP_LEFT_RIGHT)
 flip_img.show()
 
