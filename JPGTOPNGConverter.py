b20bdc53ab6e3d3ba0c0b8bbc6b7a927333e2411
Remiya <remiya333@gmail.com>
2020-06-24 12:58:54 +0530

    Code completed and tested

--- JPGTOPNGConverter.py
+++ JPGTOPNGConverter.py
@@ -1,3 +1,26 @@
 import sys
 import os
 from PIL import Image
+
+'''
+This Code Converts the images in JPG to PNG format. It takes the input and output path from the commandline as commandline args.
+@input folder = args[1]
+@output folder = args[2]
+'''
+#get the input folder and output folder from commandline
+input_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+#check if output folder exists if not create the folder
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+#loop through the input file and save the converted file to output folder
+for filename in os.listdir(input_folder):
+
+    img = Image.open(input_folder+filename)
+    cleanname = os.path.splitext(filename)[0]
+    img.save(output_folder+cleanname+'.png','png')
+    print('done')
+
+#img = Image.open(f'{input_folder}{filename}')
